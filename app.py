15ef9730b9f0cc49e2a3a1071a9e5ce632447d08
Jorge Mendes <mendesjorge49@gmail.com>
2019-04-20 16:31:01 +0100

    change in app flask

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from time_matters import timeMatters
+import os
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':  
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port)
