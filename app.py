9d08507fa8285a7fee4280aa2cf0820b443e3c65
Jorge Mendes <mendesjorge49@gmail.com>
2019-04-21 23:03:14 +0100

    Creation of get method for api of time Matters

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask
 from time_matters import timeMatters
+from flask import Flask, request
+from flask_restplus import Api, Resource, fields
 import os
 
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Hello, World!"
+flask_app = Flask(__name__)
+app = Api(app=flask_app)
 
 
-if __name__ == '__main__':  
-    app.debug = True
+name_space = app.namespace('Time_Matters', description='get relevant dates and his score form a text')
+@name_space.route("/<string:text>")
+class MyResource(Resource):
+    @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
+             params={'text': 'Insert text', 'max_distance': 'max distance between words',
+                     'threshold': 'minimum value allow of dice calculation (default 0.05)',
+                     'max_array_length': 'max length considered of vector'})
+    def get(self, text):
+        max_distance = int(request.args.get('max_distance', 5))
+        threshold = float(request.args.get('threshold', 0.05))
+        max_array_length = int(request.args.get('max_distance', 0))
+        arr = timeMatters(text, max_distance, threshold, max_array_length)
+
+        json_dates = format_data(arr)
+        return {'Value': json_dates}
+
+
+def format_data(dates_list):
+    json_dates = []
+    for i in range(len(dates_list)):
+        json_dates.append({'date': dates_list[i][0], 'score': dates_list[i][1]})
+    return json_dates
+
+
+if __name__ == '__main__':
+    flask_app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    flask_app.run(host='127.0.0.1', port=port)
