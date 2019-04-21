913035c4c5f5d8141ac8e2af00140789704ce846
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-04-22 00:41:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def format_data(dates_list):
 
 
 if __name__ == '__main__':
-    nltk.download('punkt')
+    nltk.download('all')
     flask_app.debug = True
     port = int(os.environ.get("PORT", 443))
     flask_app.run(host='0.0.0.0', port=port)
