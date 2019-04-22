ed82cf9e93a90b6c47363faee91610806a3c8348
Jorge Mendes <mendesjorge49@gmail.com>
2019-04-22 13:11:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from time_matters import timeMatters
 from flask import Flask, request
 from flask_restplus import Api, Resource, fields
 import os
-import nltk
 
 flask_app = Flask(__name__)
 app = Api(app=flask_app)
@@ -27,14 +26,11 @@ class MyResource(Resource):
 def format_data(dates_list):
     json_dates = []
     for i in range(len(dates_list)):
-        for k in range(len(dates_list[i])):
-            json_dates.append({'date': dates_list[i], 'score': dates_list[i][1]})
-    print(json_dates)
+        json_dates.append({'date': dates_list[i][0], 'score': dates_list[i][1]})
     return json_dates
 
 
 if __name__ == '__main__':
-    nltk.download('all')
     flask_app.debug = True
     port = int(os.environ.get("PORT", 443))
     flask_app.run(host='0.0.0.0', port=port)
