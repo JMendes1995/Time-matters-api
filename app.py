7d4c25a7ca3fbdcbe28bb822575ce136cbc7ad4c
Jorge Mendes <mendesjorge49@gmail.com>
2019-05-04 13:36:34 +0100

    addition of time matters per sentence

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
-from time_matters import timeMatters
+from time_matters import timeMatters, timeMattersPerSentence
 from flask import Flask, request
-from flask_restplus import Api, Resource, fields
+from flask_restplus import Api, Resource, fields, inputs
 import os
 
 flask_app = Flask(__name__)
 app = Api(app=flask_app)
 
+bool_inputs_text = app.parser()
+bool_inputs_text.add_argument('analysis_sentence', type=inputs.boolean, default=True)
+bool_inputs_text.add_argument('ignore_contextual_window_distance', type=inputs.boolean, default=False)
 name_space = app.namespace('Time_Matters', description='get relevant dates and his score form a text')
 @name_space.route("/")
 class MyResource(Resource):
     @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
-             params={'text': 'Insert text', 'max_distance': 'max distance between words',
-                     'threshold': 'minimum value allow of dice calculation (default 0.05)',
-                     'max_array_length': 'max length considered of vector'})
+             params={'text': 'Insert Text',
+                     'contextual_window_distance': 'max distance between words',
+                     'Th': 'th is the minimum DICE threshold similarity value that defines the terms that are part of the contexto vector',
+                     'N': 'N is the size of the context vector',
+                     'max_keywords': 'define max keywords'})
+    @app.expect(bool_inputs_text)
     def get(self):
         text = str(request.args.get('text'))
-        max_distance = int(request.args.get('max_distance', 5))
-        threshold = float(request.args.get('threshold', 0.05))
-        max_array_length = int(request.args.get('max_distance', 0))
-        arr = timeMatters(text, max_distance, threshold, max_array_length)
+        max_distance = int(request.args.get('contextual_window_distance', 10))
+        threshold = float(request.args.get('Th', 0.05))
+        max_array_length = int(request.args.get('N', 0))
+        max_keywords = int(request.args.get('max_keywords', 10))
+        data = bool_inputs_text.parse_args()
 
-        json_dates = format_data(arr)
-        return {'Value': json_dates}
+        json_dates = timeMatters(text, max_distance, threshold, max_array_length, max_keywords, data['analysis_sentence'], data['ignore_contextual_window_distance'])
+        return json_dates
 
 
-def format_data(dates_list):
-    json_dates = []
-    for i in range(len(dates_list)):
-        json_dates.append({'date': dates_list[i][0], 'score': dates_list[i][1]})
-    return json_dates
+bool_inputs_sentence = app.parser()
+bool_inputs_sentence.add_argument('ignore_contextual_window_distance', type=inputs.boolean, default=False)
+name_space = app.namespace('Time_Matters_per_sentence', description='get relevant dates and his score per sentence')
+@name_space.route("/")
+class MyResource(Resource):
+    @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
+             params={'text': 'Insert Text',
+                     'contextual_window_distance': 'max distance between words',
+                     'Th': 'th is the minimum DICE threshold similarity value that defines the terms that are part of the contexto vector',
+                     'N': 'N is the size of the context vector',
+                     'max_keywords': 'define max keywords'})
+    @app.expect(bool_inputs_sentence)
+    def get(self):
+        text = str(request.args.get('text'))
+        max_distance = int(request.args.get('contextual_window_distance', 10))
+        threshold = float(request.args.get('Th', 0.05))
+        max_array_length = int(request.args.get('N', 0))
+        max_keywords = int(request.args.get('max_keywords', 10))
+        data = bool_inputs_sentence.parse_args()
 
+        json_dates = timeMattersPerSentence(text, max_distance, threshold, max_array_length, max_keywords, data['ignore_contextual_window_distance'])
+        return json_dates
 
 if __name__ == '__main__':
     flask_app.debug = True
